ff7c415876ad8fdeb9a9f323166c1953d9073d92
rosemm <hartmanr1@chop.edu>
2024-03-01 11:54:35 -0500

    Save ability plot

--- for_organizers/r102_attendance.R
+++ for_organizers/r102_attendance.R
@@ -44,7 +44,15 @@ r102 |>
 
 r102 |> 
   tidyr::pivot_longer(missing_values:ggplot2, names_to = "skill", values_to = "rating") |> 
-  ggplot(aes(x = rating, fill = skill)) + 
-  geom_histogram(bins = 4, show.legend = FALSE) + 
-  facet_wrap(~skill) + 
-  theme_classic()
+  ggplot(aes(y = rating, fill = skill)) + 
+  geom_histogram(bins = 5, show.legend = FALSE) + 
+  facet_wrap(~skill, ncol = 1) + 
+  theme_classic() + 
+  scale_y_continuous(breaks = 1:4, limits = c(.5,4.5),
+                                     labels = c("(1) I wouldn't know where to start",
+                                                "(2) I could struggle through,\nbut not confident I could do it",
+                                                "(3) I could probably do it\nwith some trial and error",
+                                                "(4) I am confident in my ability to do it")) + 
+  labs(x=NULL, y=NULL,
+       title = paste0("Signups as of ", Sys.Date()))
+ggsave("ability_plots.png", height = 10, width = 5, units = "in")
